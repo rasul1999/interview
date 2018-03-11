0a00fb113c1378e003eca6634e1bfa87ce73f1e8
rasul1999 <rasulnabiyev59@gmail.com>
2018-03-12 00:47:20 +0400

    Update Longest Common Subsequence.cpp

--- C++/Dynamic Programming/Longest Common Subsequence.cpp
+++ C++/Dynamic Programming/Longest Common Subsequence.cpp
@@ -4,7 +4,7 @@ int lcs(string x,string y){
     for(int i=0;i<=m;i++){
         dp[i][0] = 0;
     }
-    for(int j=0;j<=m;j++){
+    for(int j=0;j<=n;j++){
         dp[0][j] = 0;
     }
     for(int i=1;i<=m;i++){
